3af595da9d9566d47b38b6888f85f38eddb53096
Hugo Nogueira <hugo@masondigital.co.uk>
2014-02-27 12:23:59 +0000

    jQuery version
    Asks if user wants to install jQuery and if so, what version.
    Adds the corresponding version to the bower.json

---
+++ README.md
@@ -0,0 +1,46 @@
+# generator-autobot [![Build Status](https://secure.travis-ci.org/marabyte/generator-autobot.png?branch=master)](https://travis-ci.org/marabyte/generator-autobot)
+
+> [Yeoman](http://yeoman.io) generator
+
+
+## Getting Started
+
+### First things first!
+
+Autobot has nothing to do with Michael Bay or the Transformer's franchise.
+If you were looking forward to see big explosions, big explosions in slow motion, Megan Fox, Megan fox running in slow motion, or any kind of robots in desguise you're in the wrong place.
+
+Autobot is Yeoman generator that will, hopefully, help you to setting up your frontend webprojects.
+
+### What do you need
+
+Make sure you have the latest version of [Node](http://nodejs.org/) installed.
+
+Then install Yeoman](http://yeoman.io) using the command line:
+
+```
+$ npm install -g yo
+```
+
+To install generator-autobot from npm, run:
+
+```
+$ npm install -g generator-autobot
+```
+
+Finally, initiate the generator:
+
+```
+$ yo autobot
+```
+
+### How to use Autobot
+
+After initializing Autobot just answer the questions and select the tools you'll need.
+Your friendly Autobot will setup everything for you so you can spent more time coding and less time repeating boring tasks.
+
+
+
+## License
+
+MIT
---
+++ app/index.js
@@ -0,0 +1,79 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+
+var AutobotGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.npmInstall();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // have Yeoman greet the user
+    console.log(this.yeoman);
+
+    // replace it with a short and sweet description of your generator
+    console.log(chalk.magenta('You\'re using the fantastic Mason Digital\'s Autobot generator.'));
+
+    var prompts = [{
+      name: 'siteName',
+      message: 'What is the name of your new project?'
+    }, {
+      type: 'confirm',
+      name: 'includeJQuery',
+      message: 'Do you want to include jQuery?',
+      default: true
+    }, {
+      type: 'list',
+      name: 'jQueryVersion',
+      message: 'What version do you want to include?' + '\n\n' + '(Remember, jQuery 2.x has no support for lte IE8)',
+      choices: [{
+        name: 'jQuery 1.x',
+        value: '1x'
+      }, {
+        name: 'jQuery 2.x',
+        value: '2x'
+      }],
+      default: '2x',
+      when: function (props) {
+        return props.includeJQuery;
+      }
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.siteName = props.siteName;
+      this.includeJQuery = props.includeJQuery;
+      this.jQueryVersion = props.jQueryVersion;
+
+      done();
+    }.bind(this));
+  },
+
+  app: function () {
+    this.mkdir('app');
+    this.mkdir('app/styles');
+    this.mkdir('app/scripts');
+    this.mkdir('app/images');
+    this.mkdir('app/fonts');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+  },
+
+  projectfiles: function () {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+  }
+});
+
+module.exports = AutobotGenerator;
